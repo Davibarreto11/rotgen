6a0862d3f12d71769d0a47e1fd09c02e10d281e3
Davi Barreto <daviarturdesa686@gmail.com>
2024-04-02 14:37:28 -0300

    Configuration project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,18 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "cor-bg": "#81b398",
       },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+      "4xl": "2560px",
+      "5xl": "3840px",
     },
   },
   plugins: [],
